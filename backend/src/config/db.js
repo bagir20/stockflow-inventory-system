1716f0492c2ba944847f97dceb9cd6c27d12666f
Muhammad Bagir <mdbagir20@gmail.com>
2026-03-17 23:34:02 +0700

    ready deploy render

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,25 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
-  host:     process.env.DB_HOST,
-  port:     process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-})
+const isProduction = process.env.DATABASE_URL
 
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+      }
+)
+
+// test koneksi
 pool.connect()
   .then(() => console.log('PostgreSQL connected'))
   .catch(err => console.error('DB connection error:', err.message))
